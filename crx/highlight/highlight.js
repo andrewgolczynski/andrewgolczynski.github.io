f0f2c49442cb95c8984fa2c7966eb15afb975881
Andrew Golczynski <andrew.golczynski@appian.com>
2015-12-28 19:04:28 -0500

    Adding source files and .crx for click to highlight.

---
+++ crx/highlight/highlight.js
@@ -0,0 +1,95 @@
+// Original Hilitor JavaScript code by Chirp Internet: www.chirp.com.au
+// Please acknowledge use of this code by including this header.
+// Modified by Andrew Golczynski (error handling, specialization for crx deployment)
+
+function Hilitor(id, tag)
+{
+
+  var targetNode = document.getElementById(id) || document.body;
+  var hiliteTag = tag || "EM";
+  var skipTags = new RegExp("^(?:" + hiliteTag + "|SCRIPT|FORM|SPAN)$");
+  var color = "#ff9900";
+  var wordColor = "";
+  var matchRegex = "";
+  var openLeft = false;
+  var openRight = false;
+
+  this.setRegex = function(input)
+  {
+    input = input.replace(/^[^\w]+|[^\w]+$/g, "").replace(/[^\w'-]+/g, "|");
+    if (input == "") {return 0;}
+    var re = "(" + input + ")";
+    if(!this.openLeft) re = "\\b" + re;
+    if(!this.openRight) re = re + "\\b";
+    matchRegex = new RegExp(re, "i");
+  };
+
+  this.getRegex = function()
+  {
+    var retval = matchRegex.toString();
+    retval = retval.replace(/(^\/(\\b)?|\(|\)|(\\b)?\/i$)/g, "");
+    retval = retval.replace(/\|/g, " ");
+    return retval;
+  };
+
+  // recursively apply word highlighting
+  this.hiliteWords = function(node)
+  {
+    if(node === undefined || !node) return;
+    if(!matchRegex) return;
+    if(skipTags.test(node.nodeName)) return;
+
+    if(node.hasChildNodes()) {
+      for(var i=0; i < node.childNodes.length; i++)
+        this.hiliteWords(node.childNodes[i]);
+    }
+    if(node.nodeType == 3) { // NODE_TEXT
+      if((nv = node.nodeValue) && (regs = matchRegex.exec(nv))) {
+        wordColor = color;
+
+        var match = document.createElement(hiliteTag);
+        match.appendChild(document.createTextNode(regs[0]));
+        match.style.backgroundColor = wordColor;
+        match.style.fontStyle = "inherit";
+        match.style.color = "#000";
+
+        var after = node.splitText(regs.index);
+        after.nodeValue = after.nodeValue.substring(regs[0].length);
+        node.parentNode.insertBefore(match, after);
+      }
+    };
+  };
+
+  // remove highlighting
+  this.remove = function()
+  {
+    var arr = document.getElementsByTagName(hiliteTag);
+    while(arr.length && (el = arr[0])) {
+      var parent = el.parentNode;
+      parent.replaceChild(el.firstChild, el);
+      parent.normalize();
+    }
+  };
+
+  // start highlighting at target node
+  this.apply = function(input)
+  {
+    this.remove();
+    if(input === undefined || !input) return;
+    if(this.setRegex(input) == 0) {return;}
+    this.hiliteWords(targetNode);
+  };
+
+}
+
+var hi = new Hilitor();
+
+function highlightSelectedText() {
+  var sel = window.getSelection().toString();
+  hi.apply(sel);
+}
+
+document.addEventListener("dblclick", highlightSelectedText);
+window.oncontextmenu = function() {
+  hi.remove();
+}
---
+++ crx/highlight/readme.txt
@@ -0,0 +1,8 @@
+Readme.txt
+
+This .crx (Chrome extension file) works as-follows:
+
+- Double-click on any word to highlight it anywhere in the current document
+- To dismiss the current highlight, open the context menu (right click).  A single click will not remove the highlighting.
+- Only one highlight is possible at a time to prevent confusion - making another highlight selection (via double-click) will remove the old highlighting
+- Double-clicking whitespace or symbols will not cause a highlight to appear - non-word inputs are ignored
